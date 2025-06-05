9454e579b7e32d9c31d3d374b6b66492ee8fca17
loneLou526 <103879098+loneLou526@users.noreply.github.com>
2025-06-06 02:16:27 +0300

    clear draft.py

--- draft.py
+++ draft.py
@@ -2,7 +2,7 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-password = "hello"  # Здесь укажи нужный пароль
+password = "hello"  
 hashed_password = pwd_context.hash(password)
 
 print("Хэш пароля:", hashed_password)
