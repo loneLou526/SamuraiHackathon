de1f5a13599c238bf9867721506a1886396b3c17
loneLou526 <103879098+loneLou526@users.noreply.github.com>
2025-06-06 02:14:38 +0300

    clear task.py

--- backend/models/task.py
+++ backend/models/task.py
@@ -12,12 +12,3 @@ class Task(Base):
     image_filename = Column(String, nullable=True, default=None)
     download_filename = Column(String, nullable=True, default=None)
     download_display_name = Column(String, nullable=True, default=None)
-
-
-"""
-id	question	answer
-1	Ты лох?	Да
-2	2 + 2=	хз
-3	ты выиграл?	железно
-4	ну че?	ниче
-"""
